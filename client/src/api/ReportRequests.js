e7461ca2dc976b9df47ac5e47cb1d3ad5a653342
Eric Gauzens <ericgauzens@hotmail.com>
2021-03-21 17:52:42 -0400

    cleaned up ajax call

--- client/src/api/ReportRequests.js
+++ client/src/api/ReportRequests.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const fetchReportInput = async () => {
-	const config = { headers: { Accept: "application/json" } };
-	const res = await axios.get("/api/report_input", config);
-	return res;
+	return await axios.get("/api/report_input");
 };
